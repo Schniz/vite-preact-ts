2f2c9302387dca4df7283af9df1a8b863c91963a
Gal Schlezinger <gal@spitfire.co.il>
2021-01-23 09:59:22 +0200

    find new issue in v...35

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,4 @@
 import preactRefresh from '@prefresh/vite'
-import analyzer from "rollup-plugin-analyzer";
-import visualizer from "rollup-plugin-visualizer";
 import { defineConfig } from 'vite'
 
 export default defineConfig({
@@ -13,9 +11,4 @@ export default defineConfig({
     react: 'preact/compat',
     'react-dom': 'preact/compat'
   },
-  build: {
-    rollupOptions: {
-      plugins: [visualizer(), analyzer()]
-    }
-  }
 })
